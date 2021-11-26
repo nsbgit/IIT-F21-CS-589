ec31b18adee5d52693147091a3632581cfb1c64f
Sukanta Sharma <nsbgit@users.noreply.github.com>
2021-11-25 21:21:36 -0600

    javadoc logout

--- Project/EclipseWorkspace/AccountClassTestDriver/src/account.java
+++ Project/EclipseWorkspace/AccountClassTestDriver/src/account.java
@@ -111,9 +111,8 @@ public class account {
 	}
 
 	/**
-	 * The Desciption of the method to explain what the method does
+	 * allows to logout from the account
 	 * 
-	 * @param the parameters used by the method
 	 * @return the value returned by the method  0 --> Successful, -1 --> ERROR
 	 */
 	public final int logout() {
