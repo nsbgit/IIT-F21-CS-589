ec9f0e41b6b90733e32a95ae2d3c7221e6010256
Sukanta Sharma <nsbgit@users.noreply.github.com>
2021-11-25 23:19:56 -0600

    Get Test Case at the end of the progream

--- Project/EclipseWorkspace/AccountClassTestDriver/src/TestDriver.java
+++ Project/EclipseWorkspace/AccountClassTestDriver/src/TestDriver.java
@@ -83,6 +83,7 @@ public class TestDriver {
 		
 		System.out.println("Quitting Account Driver...");
 		System.out.println(strLine + "\n\t\tTHANK YOU!!!\n" + strLine);
+		TestSuiteData.getTestCase();
 	}
 
 	private static String showMenu() {
