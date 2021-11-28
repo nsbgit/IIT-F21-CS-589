f198e4a2a14a3092442f7888b9ce0fe99c1f8331
Sukanta Sharma <nsbgit@users.noreply.github.com>
2021-11-28 05:11:26 -0600

    comment

--- Project/EclipseWorkspace/AccountClassTestDriver/src/account.java
+++ Project/EclipseWorkspace/AccountClassTestDriver/src/account.java
@@ -105,7 +105,7 @@ public class account {
 	}
 
 	/**
-	 * Constructor
+	 * Constructor for parameters initialization
 	 */
 	public account() {
 		x2 = 0;
@@ -147,7 +147,7 @@ public class account {
 		if (x4 != 1) { // Check if the current state is CHECK PIN
 			return -1;
 		}
-		if (x == x3) { // TODO BUG T8 T16
+		if (x == x3) {
 			x4 = 2;
 			return 0;
 		} else {
